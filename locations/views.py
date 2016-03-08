4cc70be66e4f8f3e2f059da8c1693d0b68e0a433
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2016-03-08 22:28:07 +0100

    Show the list of addresses after the map

--- locations/views.py
+++ locations/views.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
 
+
+from .models import Location
+
+
 class HomeView(TemplateView):
     template_name = "locations/index.html"
+
+    def get_context_data(self, **kwargs):
+        # Include the already stored addresses when reloading
+        context = super().get_context_data()
+        context['locations'] = Location.objects.all()
+        return context
